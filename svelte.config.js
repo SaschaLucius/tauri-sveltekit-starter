c66861c47e9fa0bc48ca52bbf8fee1c1fdd04813
Sascha Lucius <sascha.lucius@datameer.com>
2021-11-16 09:39:58 +0100

    Added tailwind css

--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,4 @@
+import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,7 +12,13 @@ const config = {
 		ssr: false,
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
-	}
+	},
+
+	preprocess: [
+		preprocess({
+			postcss: true
+		})
+	]
 };
 
 export default config;
---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+const config = {
+	mode: 'jit',
+	// purge: ['./src/**/*.{html,js,svelte,ts}'],
+	purge: ['./src/**/*.{html,js,svelte}'],
+
+	theme: {
+		extend: {}
+	},
+
+	plugins: []
+};
+
+module.exports = config;
