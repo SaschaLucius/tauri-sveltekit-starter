394f2d7b5faf0e243e531046f8b33e2a3b05c092
Sascha Lucius <sascha.lucius@datameer.com>
2021-11-29 09:20:00 +0100

    cleanup

--- svelte.config.js
+++ svelte.config.js
@@ -3,33 +3,36 @@ import staticAdapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    // Consult https://github.com/sveltejs/svelte-preprocess
-    // for more information about preprocessors
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
 	preprocess: sveltePreprocess({
 		// in auto-preprocessing mode, you can set postcss: true if postcss-load-config is installed and svelte-preprocess will look for a PostCSS config file in your project.
-        postcss: true
-    }),
+		postcss: true
+	}),
 
-    kit: {
-        adapter: staticAdapter({
-            // default options are shown
-            pages: 'build',
-            assets: 'build',
+	kit: {
+		adapter: staticAdapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
 			fallback: null
-        }),
-        // hydrate the <div id="svelte"> element in src/app.html
-        target: '#svelte',
+		}),
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte',
 		files: {
 			template: 'src/app.html'
-		  },
-		  
-        ssr: false,
-        vite: {
-            ssr: {
-                noExternal: true,
-            }
-        }
-    }
+		},
+
+		// disables server-side rendering app-wide
+		ssr: false,
+		// A Vite config object, or a function that returns one. Not all configuration options can be set, since SvelteKit depends on certain values being configured internally.
+		vite: {
+			ssr: {
+				// bundle your SSR build into a single JavaScript file (Treat all dependencies as noExternal. Throw an error if any Node.js built-ins are imported)
+				noExternal: true
+			}
+		}
+	}
 };
 
 export default config;
